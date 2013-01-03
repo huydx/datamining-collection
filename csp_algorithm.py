737ed2bbd29bd4275ef4ca9ddc0b2a634b4cd701
dxhuy1988 <doxuanhuy@gmail.com>
2013-01-04 02:43:42 +0900

    complete csp

--- csp_algorithm.py
+++ csp_algorithm.py
@@ -8,6 +8,8 @@ wave_r = None
 fft_wave_l = None
 fft_wave_r = None
 csp = None
+degree = 0
+degree_n = 0
 
 def input():
   global wave_l, wave_r
@@ -15,14 +17,16 @@ def input():
   wave_r = sp.loadtxt('right.dat', usecols=(1,), unpack=True)
  
 def _unittest():
-  print fft_wave_l
-  print fft_wave_r
+  #print fft_wave_l
+  #print fft_wave_r
   print csp
-
+  print degree
+  print degree_n
+  
 if __name__ == '__main__':
   input()
-  fft_wave_l = fft(wave_l)
-  fft_wave_r = fft(wave_r) 
+  fft_wave_l = fft(wave_l) #fourier transform of left wave
+  fft_wave_r = fft(wave_r) #fourier transform of right wave
   
   sz_l = fft_wave_l.shape[0]
   sz_r = fft_wave_r.shape[0]
@@ -33,17 +37,49 @@ if __name__ == '__main__':
   for i in range(fft_wave_l.shape[0]):
     dividend = fft_wave_l[i] * (fft_wave_r[i].conj())
     divisor = abs(fft_wave_l[i]) * abs(fft_wave_r[i])
-    csp[i] = dividend / divisor
+    csp[i] = dividend / divisor ##calculate Cross-power Spectrum Phase analysis
 
   csp = ifft(csp)
-  csp = sp.apply_along_axis(lambda x: abs(x), 0, csp)
+  a_csp = sp.apply_along_axis(lambda x: abs(x), 0, csp)
+  max = a_csp.max(0)/16000 ##max of argument
+
+  degree = sp.arccos(max*34000/10) ##34000: speed of sound wave, 10: distance of microphone array
+
+  ##find noise direction
+  ##delete 2 max value
+  max_idx = a_csp.argmax(0)
+  a_csp[max_idx] = 0
+  max_idx = a_csp.argmax(0)
+  a_csp[max_idx] = 0
+  
+  #find second max value 
+  max_n = a_csp.max(0)/16000 #noise time interval
+  max_n_idx = a_csp.argmax(0)
+  degree_n = sp.arccos(max_n*34000/10)
+
+  ##shift left wave to max_n_idx to normalize
+  noise_nml_wave_l = sp.roll(wave_l, max_n_idx, 0)   
+  for i in range(max_n_idx):
+    noise_nml_wave_l[i] = 0
+
+  ##substract right wave
+  substract_wave = noise_nml_wave_l
+  for i in range(sz_l):
+    substract_wave[i] = wave_r[i] + noise_nml_wave_l[i]
+
+  time_axis = sp.loadtxt('left.dat', usecols=(0,), unpack=True)
+  output = sp.zeros((2, sz_l))
+  output[0] = time_axis
+  output[1] = noise_nml_wave_l
+  output = sp.transpose(output)
+
+  sp.savetxt('output.dat', output, fmt='%.10f', delimiter='\t', newline='\n')
+
+  #_unittest()
   
   plotdt = sp.zeros((2, sz_l))
   plotdt[0] = sp.loadtxt("left.dat", usecols=(0,), unpack=True)
   plotdt[1] = csp
   
-  plt.scatter(plotdt[0], plotdt[1], color='r')
+  plt.plot(plotdt[0], plotdt[1], color='r', linewidth=0.5)
   plt.show()
-  #print t
-  #_unittest()
-
